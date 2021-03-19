df20be9f56da5c5028726038ef61d972803f1cd5
nelvson <nelvson1337@gmail.com>
2021-03-19 08:14:09 +0700

    Create Modal component

---
+++ src/general/components/Modal.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode, CSSProperties } from 'react';
+import { StyleSheet, StyleProp, ViewStyle } from 'react-native';
+import { Dialog, DialogContent, makeStyles } from '@material-ui/core';
+
+export type Props = {
+  children: ReactNode;
+  open: boolean;
+  onClose: () => void;
+  style?: StyleProp<ViewStyle>;
+  contentStyle?: StyleProp<ViewStyle>;
+  fullWidth?: boolean;
+};
+
+const useStyles = makeStyles({
+  paperFullWidth: {
+    maxWidth: '68%',
+  },
+});
+
+export default function Modal(props: Props) {
+  let classes = useStyles();
+  let { children, open, style, contentStyle, onClose, fullWidth } = props;
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      fullWidth={fullWidth}
+      classes={classes}
+      style={StyleSheet.flatten(style) as CSSProperties}
+    >
+      <DialogContent
+        style={
+          StyleSheet.flatten([styles.spacing, contentStyle]) as CSSProperties
+        }
+      >
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+const styles = StyleSheet.create({
+  spacing: {
+    padding: 40,
+  },
+});
