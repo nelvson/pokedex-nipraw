e1a91fd9860001ee4d92cda338148e0e640297c2
nelvson <nelvson1337@gmail.com>
2021-03-21 03:45:30 +0700

    Work on Landing screen

--- src/screens/Landing.tsx
+++ src/screens/Landing.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { TextField } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+import { useHistory } from 'react-router';
+
+const SearchBar = (props: { func: Function }) => {
+  let [text, setText] = useState('');
+  return (
+    <View style={styles.textEntry}>
+      <TextField
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            props.func(`/detail/${text}`);
+          }
+        }}
+        label="Search a pokemon here..."
+        value={text}
+        style={{ width: 300 }}
+        onChange={(event) => setText(event.target.value)}
+      />
+      <Search onClick={() => props.func(`/detail/${text}`)} />
+    </View>
+  );
+};
 
 export default function Landing() {
+  let history = useHistory();
+
   return (
-    <View>
-      <Text>hello there, Landing</Text>
+    <View style={styles.root}>
+      <View style={styles.searchBar}>
+        <SearchBar func={history.push} />
+      </View>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  searchBar: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center', //changed value of alignItems
+  },
+  textEntry: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
